5ed45d1c5f6f2fc724bf3e7eb875b06760b2e12d
Matteo Golfetto <Matteogol16@gmail.com>
2026-03-25 20:22:28 +0100

    Fix Mouse Wheel interaction

---
+++ Source/ProjectHyde/Core/Subsystems/UI/MouseHoverSubsystem.cpp
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MouseHoverSubsystem.h"
+#include "Components/Viewport.h"
+
+
+
+bool UMouseHoverSubsystem::IsMouseInsideWidget(UUserWidget* Widget)
+{
+	FVector2D Local = GetMousePositionInWidget(Widget);
+	FVector2D Size = Widget->GetCachedGeometry().GetLocalSize();
+
+	return Local.X >= 0 && Local.Y >= 0 &&
+		   Local.X <= Size.X && Local.Y <= Size.Y;
+}
+
+FVector2D UMouseHoverSubsystem::GetMousePositionInWidget(UUserWidget* Widget)
+{
+	if (!Widget) return FVector2D::ZeroVector;
+
+	FGeometry Geometry = Widget->GetCachedGeometry();
+
+	FVector2D CursorPos = FSlateApplication::Get().GetCursorPos();
+
+	return Geometry.AbsoluteToLocal(CursorPos);
+}
---
+++ Source/ProjectHyde/Core/Subsystems/UI/MouseHoverSubsystem.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TaskSyncManager.h"
+#include "Blueprint/GameViewportSubsystem.h"
+#include "MouseHoverSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTHYDE_API UMouseHoverSubsystem : public ULocalPlayerSubsystem   
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable)
+	bool IsMouseInsideWidget(UUserWidget* Widget);
+	
+private:
+	FVector2D GetMousePositionInWidget(UUserWidget* Widget);
+};
