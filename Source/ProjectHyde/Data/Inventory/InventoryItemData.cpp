9f950485032bfb0703f2c9e183862b4d0402bc60
Matteo Golfetto <Matteogol16@gmail.com>
2026-09-08 23:06:09 +0200

    sistemati gli item (mancano le traduzioni)

--- Source/ProjectHyde/Data/Inventory/InventoryItemData.cpp
+++ Source/ProjectHyde/Data/Inventory/InventoryItemData.cpp
@@ -1,2 +1,26 @@
 ﻿
 #include "InventoryItemData.h"
+
+#include "Internationalization/Culture.h"
+
+void UInventoryItemData::PostInitProperties()
+{
+	Super::PostInitProperties();
+	
+	TArray<FString> currentCultures =	
+		FTextLocalizationManager::Get().GetLocalizedCultureNames(ELocalizationLoadFlags::Game);
+	
+	for (auto CurrentCulture : currentCultures)
+	{
+		if (!Text.Contains(CurrentCulture))
+		{
+			Text.Add(CurrentCulture, nullptr);
+		}
+	}
+}
+
+UTexture2D* UInventoryItemData::GetTextForCurrentCulture()
+{
+	FString currentCulture = FInternationalization::Get().GetCurrentLanguage()->GetName();
+	return Text[currentCulture];
+}
--- Source/ProjectHyde/Data/Inventory/InventoryItemData.h
+++ Source/ProjectHyde/Data/Inventory/InventoryItemData.h
@@ -8,10 +8,14 @@ UCLASS(BlueprintType)
 class PROJECTHYDE_API UInventoryItemData : public UPrimaryDataAsset
 {
 	GENERATED_BODY()
-
+	
+private:
+	virtual void PostInitProperties() override;
+	
+	
 public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
-	FName Name = FName();
+	FText Name = FText();
 	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
 	FText Description = FText();
@@ -19,8 +23,8 @@ public:
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Item")
 	bool bHasText = false;
 	
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Item")
-	UTexture2D* Text = nullptr;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Item",meta = (EditCondition = "bHasText", EditConditionHides))
+	TMap<FString,UTexture2D*> Text;
 	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
 	UTexture2D* Icon = nullptr;
@@ -30,4 +34,7 @@ public:
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
 	UNotebookItemData* NotebookDataAsset = nullptr;
+	
+	UFUNCTION(BlueprintCallable, Category = "Item")
+	UTexture2D* GetTextForCurrentCulture();
 };
